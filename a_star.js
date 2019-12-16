a7aa36882424da9c695f7ca6169f1a125e3a5959
Anirban Mukherjee <raji08xd@gmail.com>
2019-12-16 14:59:16 -0800

    Update a_star.js
    - Manhattan Distance

--- a_star.js
+++ a_star.js
@@ -1,7 +1,15 @@
 goal=[[0,1,2],[3,4,5],[6,7,8]]
 
+//Heuristic
 function h(grid){
-	//Returns Heuristic value
+	//Manhattan Distance
+	sum=0;
+	for(i=0;i<3;i++){
+		for(j=0;j<3;j++){
+			hi=Math.abs(i-Math.floor(grid[i][j]/3))+Math.abs(j-grid[i][j]%3)
+			sum+=hi
+		}
+	}
 }
 
 function a_star(grid){
