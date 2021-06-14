2301dcc1d47efb52232f8dc7316e3f1c4c23296f
Anirban Mukherjee <raji08xd@gmail.com>
2021-06-14 13:20:27 +0530

    Update a_star.js
    Fixed comments

--- a_star.js
+++ a_star.js
@@ -1,5 +1,4 @@
-// STATE FORMAT:
-// [ grid, f-score, direction, parent ]
+///// State object contains the state grid (3x3 matrix), direction moved to get to the state, the level (number of moves to get to the state), its parents state and the f-score of the state
 
 function state(grid, direction, level, parent){
 	this.grid = grid;
@@ -10,19 +9,28 @@ function state(grid, direction, level, parent){
 }
 
 max_level=-1;
+max_f_score=-1;
 
-//Goal state	
+///// Goal state	
+// Used to check whether goal is reached
 const goal=[[0,1,2],[3,4,5],[6,7,8]]
+
+// Flag to see whether goal is reached
+// Used as a condition for iteration
 goal_reached=false;
 
 //Required moves
 moves=[]
 
-//Processing nodes
-//open_list=new Set()
 
-///// List of unvisited nodes
+///// Open list
+// List of states under consideration
 open_list=[]
+
+///// Closed list
+// List of states done considering
+closed_list=[]
+
 
 ///// Compare two grids
 // Returns true if grids are same
@@ -34,7 +42,12 @@ function compare_grids(grid1,grid2){
 	return true;
 }
 
-//Check if open list has a state
+//Check if grid is goal state
+function check_goal(grid){
+	return(compare_grids(grid,goal));
+}
+
+///// Check if open list has a grid
 function check_open(g){
 	for(open_list_i=0;open_list_i<open_list.length;open_list_i++){
 		if(compare_grids(open_list[open_list_i].grid,g)){
@@ -45,12 +58,7 @@ function check_open(g){
 	return false;
 }
 
-///// List of Visited nodes
-// CLOSE LIST format [ grid, parent, dir ]
-closed_list=[]
-
-///// Check if element exists in closed list
-// Returns true if element exists, else false
+///// Check if closed list has a grid
 function check_closed(g){
 	if(closed_list.length>0){	
 		for(closed_list_i=0;closed_list_i<closed_list.length;closed_list_i++){
@@ -62,13 +70,7 @@ function check_closed(g){
 	}
 }
 
-
-//Check if grid is goal state
-function check_goal(grid){
-	return(compare_grids(grid,goal));
-}
-
-//Convert number to direction character
+// Convert number to direction character
 function to_dir(x){
 	switch(x){
 		case(0):return('u');
@@ -80,7 +82,7 @@ function to_dir(x){
 
 
 ///// Returns value of state in number
-// Initially 012345678
+// Goal is 012345678
 
 function state_val(grid){
 	val=0;
@@ -102,15 +104,13 @@ function priority_sort(list){
 	return(list);
 }
 
-
-//Heuristic
+///// Heuristic function
+// Using Manhattan Distance
 function h(grid){
-	//Manhattan Distance
 	sum=0;
 	for(i=0;i<3;i++){
 		for(j=0;j<3;j++){
 			hi=Math.abs(i-Math.floor(grid[i][j]/3))+Math.abs(j-grid[i][j]%3)
-			//console.log('h'+(i*3+j)+'= ',hi);
 			if(grid[i][j]!=0){
 				sum+=hi;
 			}
@@ -119,34 +119,23 @@ function h(grid){
 	return sum;
 }
 
-//Backtrack path
+///// Backtrack path
 function backtrack_path(){
 	console.log("Backtrack");
 	present_state=closed_list.pop();
-	console.log(present_state, present_state.parent!=null);
+	//console.log(present_state, present_state.parent!=null);
 	while(present_state.parent!=null){
-		console.log("DIR:", present_state.direction);
+		//console.log("DIR:", present_state.direction);
 		moves.push(present_state.direction);
 		present_state=present_state.parent;
 	}
-	/*
-	// parent=state_val(present_state.parent);
-	parent=present_state.parent;
-	moves.push(present_state.direction);
-	while(closed_list.length>0){
-		present_state=closed_list.pop();
-		if(present_state==parent){
-			parent=state_val(present_state.parent);
-			moves.push(present_state.dir);
-		}
-	}
-	*/
-	console.log("moves: ", moves)
+	console.log("Number of Moves: ", moves.length)
 }
 
 
 
-///// A* function priority queue iterative
+///// A* function 
+// priority queue iterative
 
 function a_star(grid,level,last){
 	// Pushing start state
@@ -158,18 +147,22 @@ function a_star(grid,level,last){
 
 		// Sorting open_list as per f_score
 		open_list = priority_sort(open_list);
-		console.log("Best state: ", open_list[0]);
+
 		// Taking present state as first element of sorted open list
 		present_state = open_list.shift();
 
+		if(present_state.level>max_level){
+			max_level = present_state.level;
+			console.log("New max_level: ", max_level);
+		}
 
-		console.log("Current state: ", present_state);
-		console.log("Pushing closed")
+		if(present_state.f_score>max_f_score){
+			max_f_score = present_state.f_score;
+			console.log("New max_f_score: ", max_f_score);
+		}
 
+		// Pushing present state to closed_list
 		closed_list.push(present_state);
-
-		console.log("Open: ", open_list);
-		console.log("Closed: ", closed_list);
 
 		//Creating a copy of real grid 
 		var grid = present_state.grid.map(function(arr) {
@@ -181,26 +174,24 @@ function a_star(grid,level,last){
 			console.log("Goal");
 			goal_reached=true;
 			backtrack_path();
-			console.log("Moves: ",moves);
-			console.log("MAX LEVEL: ", max_level);
 			break;
 		}
 
-		//Stores results of explored paths
+		// Array to store results of explored paths
 		grid_moved=[]
+
+		// Start exploring paths from present_state
 		grid_moved[0]=gridMove(grid,'u')
 		grid_moved[1]=gridMove(grid,'r')
 		grid_moved[2]=gridMove(grid,'d')
 		grid_moved[3]=gridMove(grid,'l')
 
-		//alert(state_val(grid_moved[0]));
 
 		// Check unvisitable neighbors
 		count_null=0;
 		for(ii=0;ii<4;ii++){
 
 			//Check if explored states are in closed list
-
 			if(!grid_moved[ii] || check_closed(grid_moved[ii])){
 				grid_moved[ii]=null;
 				count_null++;
@@ -210,24 +201,21 @@ function a_star(grid,level,last){
 			}
 			
 		}
+
 		//If all neighbors are un-visitable (dead-end)
 		if(count_null==4){
-			//console.log("Dead end");
-			break;
+			continue;
 		}
-		
 
 		//Adding processing states & f_scores to OPEN LIST
 		for(ii=0;ii<4;ii++){
 			if(grid_moved[ii]){
-				console.log("Dir: ", ii);
 
 				// Creating new state
-				state_new = new state(grid_moved[ii],ii, present_state.level+1, present_state)
+				state_new = new state(grid_moved[ii], ii, present_state.level+1, present_state)
 				
 				//If open_list doesn't have new state, push to open_list
 				if(!check_open(grid_moved[ii])){
-					console.log("Pushing to open: ", state_new);
 					open_list.push(state_new);
 				}
 			}
