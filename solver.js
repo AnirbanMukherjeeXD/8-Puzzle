6d8d7c0e8573507753ed91fe188174a0e1431560
Anirban Mukherjee <raji08xd@gmail.com>
2021-06-14 13:20:42 +0530

    Update solver.js
    Added 'Solving...' message

--- solver.js
+++ solver.js
@@ -1,13 +1,16 @@
 function solve(grid){
-	a_star(grid,0);
-	moves_dir=moves.map(to_dir);
+	document.getElementById('btn_solve').innerHTML = "Solving..."
+	setTimeout(function(){a_star(grid,0);
+		moves_dir=moves.map(to_dir);
 	for(i=0;i<moves.length;i++){
-		//move(moves_dir.shift());
 		setTimeout(function(){ move(moves_dir.pop()) }, 400*i);
 	}
 	moves=[];
-	goal_reached=0;
+	goal_reached=false;
 	open_list=[];
-	//closed_list=new Set();
 	closed_list=[];
+	setTimeout(function(){ document.getElementById('btn_solve').innerHTML = "Solve using AI"; }, 400*i)
+	}, 100);
+	
+	
 }
